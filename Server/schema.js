a5e0246aff6eefa0a1cf5104b052b2c5fc196e00
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-12 14:47:08 +0530

    Update schema.js

--- Server/schema.js
+++ Server/schema.js
@@ -1,18 +1,15 @@
 const Joi = require("joi");
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
-        title : Joi.string().required(),
-        description : Joi.string().required(),  // Do we need this? Will this be AI generated?
-        image : Joi.string().allow('',null),
-        price : Joi.number().required().min(0),
-        location : Joi.string().required()
+    listing: Joi.object({
+        title: Joi.string().required(),
+        //description: Joi.string().required(),  // Do we need this? Will this be AI generated?
+        image: Joi.string().allow('', null),
+        price: Joi.number().required().min(0),
+        location: Joi.string().required()
     }).required()
 });
 
-module.exports.reviewSchema = Joi.object({
-    review : Joi.object({
-        comment : Joi.string().required(),  // Unecessary increase of hosting costs
-        rating : Joi.number().required().min(1).max(5),
-    }).required()
-})
+module.exports.ratingSchema = Joi.object({
+    rating: Joi.number().required().min(1).max(5)
+});
