7f6a653c8f9f91f3d891402f5889c1d96864a832
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-12 07:18:09 +0530

    Update schema.js
    Questions

--- Server/schema.js
+++ Server/schema.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 module.exports.listingSchema = Joi.object({
     listing : Joi.object({
         title : Joi.string().required(),
-        description : Joi.string().required(),
+        description : Joi.string().required(),  // Do we need this? Will this be AI generated?
         image : Joi.string().allow('',null),
         price : Joi.number().required().min(0),
         location : Joi.string().required()
@@ -12,7 +12,7 @@ module.exports.listingSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
     review : Joi.object({
-        comment : Joi.string().required(),
+        comment : Joi.string().required(),  // Unecessary increase of hosting costs
         rating : Joi.number().required().min(1).max(5),
     }).required()
 })
