36bc6e02bbfb1655806d46542fa75fa77cea80d9
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-12 15:01:33 +0530

    Update home.js

--- Server/routes/home.js
+++ Server/routes/home.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const hc = require("../controllers/home");
 
+router.get("/", hc.showHomePage);
+router.get("/search", hc.searchFood);
+
+module.exports = router;
