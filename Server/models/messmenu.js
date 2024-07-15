59a1aabf6e8b3d8a69c30a81d1ff0164281a832f
AnantGupta001 <anantagarwal4946@gmail.com>
2024-07-15 18:38:56 +0530

    Deleted Redundant files

--- Server/models/messmenu.js
+++ Server/models/messmenu.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const messMenuSchema = new Schema({
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const messmenuSchema = new Schema({
     date: { type: String, required: true },
-    day: { type: String, required: true },
+    day: { type: String, enum: daysOfWeek, required: true },
     breakfast: { type: String, required: true },
     lunch: { type: String, required: true },
     snacks: { type: String, required: true },
     dinner: { type: String, required: true }
 });
 
-const MessMenu = mongoose.model('MessMenu', messMenuSchema);
+const messmenu = mongoose.model('messmenu', messmenuSchema);
 
-module.exports = MessMenu;
+module.exports = messmenu;
--- Server/server.js
+++ Server/server.js
@@ -6,43 +6,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
-const ExpressError = require("./utils/ExpressError");
-const session = require("express-session");
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
 
-const listingsRouter = require("./routes/listing.js");
-const reviewsRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
-const adminRouter = require("./routes/admin.js");
-const homeRouter = require("./routes/home.js");
 const messmenuRouter = require("./routes/messmenu.js");
+const homeRouter = require("./routes/home.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
-
-const sessionOptions = {
-    secret: "Mysupersecret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    }
-}
-
-app.use(session(sessionOptions));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 main()
     .then(() => console.log("connected to DB"))
@@ -56,28 +26,8 @@ app.get("/", (req, res) => {
     res.send("Hi, I'm root route");
 });
 
-app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-});
-
-app.use("/", homeRouter);
-app.use("/listings", listingsRouter);
-app.use("/listings/:id/reviews", reviewsRouter);
-app.use("/users", userRouter);
-app.use("/admins", adminRouter);
 app.use("/messmenu", messmenuRouter);
-
-app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "PAGE NOT FOUND"));
-});
-
-app.use((err, req, res, next) => {
-    const { statusCode = 500, message = "SOMETHING WENT WRONG" } = err;
-    res.status(statusCode).render("error", { err });
-});
+app.use("/home", homeRouter);
 
 app.listen(process.env.PORT, () => {
     console.log("Server is listening...");
