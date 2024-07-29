825e26567d06ce07fd82ce1ff2f5da06ea79909f
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-29 16:15:03 +0530

    Update order.js

--- Server/models/order.js
+++ Server/models/order.js
@@ -1,32 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Order = require('./order'); 
 
-const FoodSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  rating: { type: Number, default: 0, min: 0, max: 5 },
-  price: { type: Number, required: true },
-  numberOfRatings: { type: Number, default: 0 },
-  available: { type: Boolean, default: true },
-  restaurantName: { type: String, required: true },
-  blockName: { type: String, required: true, enum: ['AB-1', 'AB-2', 'CB', 'Food Street', 'Rock Plaza', 'Maggi Point'] }
+const orderSchema = new Schema({
+    foodName: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    prepared: {
+        type: Boolean,
+        default: false
+    },
+    cancelled: {
+        type: Boolean,
+        default: false
+    }
 });
 
-const RestaurantSchema = new Schema({
-  name: { type: String, required: true },
-  rating: { type: Number, default: 0, min: 0, max: 5 },
-  totalOrders: { type: Number, default: 0 },
-  pendingOrders: { type: [Order.schema], default: [] },
-  phoneNumbers: { type: [String], required: true },
-  menu: { type: [FoodSchema], default: [] }
-});
+const Order = mongoose.model('Order', orderSchema);
 
-const BlockSchema = new Schema({
-  name: { type: String, required: true },
-  restaurants: { type: [RestaurantSchema], default: [] }
-});
-
-const Block = mongoose.model('Block', BlockSchema);
-
-module.exports = Block;
+module.exports = Order;
