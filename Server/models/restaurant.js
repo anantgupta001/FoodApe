d9bfcd63a4e5e132e91c5cca0d87b202dba0451a
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-29 16:29:36 +0530

    Create restaurant.js

---
+++ Server/models/restaurant.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Order = require('./order'); 
+const Food = require('./food');
+
+const RestaurantSchema = new Schema({
+  name: { type: String, required: true },
+  rating: { type: Number, default: 0 },
+  pendingOrders: { type: [Order.schema], default: [] }, //Can contain only OrderID and other details can be searched.
+  phoneNumbers: { type: [String], required: true },
+  menu: { type: [Food.schema], default: [] } // Can contain only food name and other details can be searched.
+});
+
+const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+module.exports = Restaurant;
