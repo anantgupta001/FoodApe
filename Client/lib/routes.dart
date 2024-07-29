767282b956a006617d965074e8c672b4d1be3a7f
Yash Jain <yashjainstudy@gmail.com>
2024-07-29 15:47:15 +0530

    Configured Home Page and other routes

---
+++ Client/lib/routes.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// Implement the routes for all the pages
+
---
+++ Client/lib/screens/cart.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: const Center(
+        child: Text('Cart Page'),
+      ),
+    );
+  }
+}
