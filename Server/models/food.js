f5e1f8173d164dc7a77b96910363bb47a3e33305
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-29 16:20:53 +0530

    Create food.js

---
+++ Server/models/food.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FoodSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  rating: { type: Number, default: 0, min: 0, max: 5 },
+  price: { type: Number, required: true },
+  numberOfRatings: { type: Number, default: 0 },
+  available: { type: Boolean, default: true },
+});
+
+module.exports = FoodSchema;
