7c891013c2a0ed2b9ef71744753b51226db2ce5c
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-29 16:09:42 +0530

    Update order.js

--- Server/models/order.js
+++ Server/models/order.js
@@ -5,17 +5,17 @@ const Order = require('./order');
 const FoodSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  rating: { type: Number, default: 0 },
+  rating: { type: Number, default: 0, min: 0, max: 5 },
   price: { type: Number, required: true },
   numberOfRatings: { type: Number, default: 0 },
   available: { type: Boolean, default: true },
   restaurantName: { type: String, required: true },
-  blockName: { type: String, required: true }
+  blockName: { type: String, required: true, enum: ['AB-1', 'AB-2', 'CB', 'Food Street', 'Rock Plaza', 'Maggi Point'] }
 });
 
 const RestaurantSchema = new Schema({
   name: { type: String, required: true },
-  rating: { type: Number, default: 0 },
+  rating: { type: Number, default: 0, min: 0, max: 5 },
   totalOrders: { type: Number, default: 0 },
   pendingOrders: { type: [Order.schema], default: [] },
   phoneNumbers: { type: [String], required: true },
