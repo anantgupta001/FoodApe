561aef055e72c94bd9be6197b17106e2e57efaf2
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-31 19:21:12 +0530

    Update food.js

--- Server/models/food.js
+++ Server/models/food.js
@@ -8,8 +8,6 @@ const FoodSchema = new Schema({
   price: { type: Number, required: true },
   numberOfRatings: { type: Number, default: 0 },
   available: { type: Boolean, default: false },
-  restaurantName: { type: String, required: true },
-  blockName: { type: String, required: true, enum: ["AB-1", "AB-2", "CB", "Food Street", "Rock Plaza", "Maggi Point"] }
 });
 
 const Food = mongoose.model('Food', FoodSchema);
