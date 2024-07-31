544ed769fce42854a2e79ee89392d0cd2dd91801
Ankit Sharma <117569772+ImmaNOOBie@users.noreply.github.com>
2024-07-31 18:52:22 +0530

    Update restaurant.js

--- Server/models/restaurant.js
+++ Server/models/restaurant.js
@@ -5,7 +5,7 @@ const Food = require('./food');
 
 const RestaurantSchema = new Schema({
   name: { type: String, required: true },
-  rating: { type: Number, default: 0 },
+  rating: { type: Number, default: 0, min: 0, max: 5 },
   pendingOrders: { type: [Order.schema], default: [] }, //Can contain only OrderID and other details can be searched.
   phoneNumbers: { type: [String], required: true },
   menu: { type: [Food.schema], default: [] } // Can contain only food name and other details can be searched.
