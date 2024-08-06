79b842c50620381a0c8d3c852236544c0ca17be7
AnantGupta001 <anantagarwal4946@gmail.com>
2024-08-06 09:36:54 +0530

    Edited messmenu dates

--- Server/controllers/messmenu.js
+++ Server/controllers/messmenu.js
@@ -1,7 +1,7 @@
 const Messmenu = require("../models/messmenu");
 
 module.exports.getMessmenuData = async (req, res) => {
-    const date = new Date().toString().split(" ")[2];
+    const date = "5";
     const messmenu = await Messmenu.findOne({ date: date });
     return { status: true, messmenu: messmenu };
 };
